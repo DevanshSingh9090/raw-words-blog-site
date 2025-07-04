9a166df3a7fe31cefc2a206e9a2ef4a56032a8f9
devansh <devanshsingh9090@gmail.com>
2025-07-04 17:35:35 +0530

    error debug

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className='text-sm text-center bg-gray-50 py-4'>
-            © Copyright 2024 | Designed & Developed By: <a href='https://www.youtube.com/@developergoswami' className='font-bold'>Developer Goswami</a>
-        </div>
-    )
-}
+  return (
+    <div className="text-sm text-center bg-gray-50 py-4">
+      © Copyright 2024 | Made by Devansh Singh
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
